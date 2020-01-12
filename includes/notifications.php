8012567a77227034f0a0bd9d46696f1d278242e3
Aro Micheal <harrowwestmykel@gmail.com>
2020-01-12 15:22:28 +0100

    Added check for empty comments

--- includes/notifications.php
+++ includes/notifications.php
@@ -703,8 +703,10 @@ function bp_like_activity_notifications_mark_read( $activity ) {
 
 	$comments = BP_Activity_Activity::get_activity_comments( $activity->id, $activity->mptt_left, $activity->mptt_right, $spam );
 
-	foreach ( $comments as $comment ) {
-		bp_notifications_mark_notifications_by_item_id( bp_loggedin_user_id(), $comment->id, buddypress()->likes->id, null );
+	if($comments != false){
+		foreach ( $comments as $comment ) {
+			bp_notifications_mark_notifications_by_item_id( bp_loggedin_user_id(), $comment->id, buddypress()->likes->id, null );
+		}		
 	}
 
 }
